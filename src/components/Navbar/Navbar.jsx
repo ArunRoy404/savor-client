ab01642c2a73068550603b4f563e40e4d1654874
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-08-10 16:16:27 +0600

    style - navbar background set

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
 // import useAuthContext from "../../CustomContexts/UseAuthContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "../Logo/Logo";
 import NavActions from "./NavActions";
 import NavLinksList from "./NavLinksList";
 import useThemeContext from "../../custom_contexts/useThemeContext";
+import { useLocation } from "react-router";
 
 
 const Navbar = () => {
     // const { loggedUser } = useAuthContext()
     const [scrolled, setScrolled] = useState(false)
-    const {isDark} = useThemeContext()
+    const { isDark } = useThemeContext()
+    const { pathname } = useLocation()
 
-    useState(() => {
+    useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
                 setScrolled(true)
@@ -27,7 +29,7 @@ const Navbar = () => {
     }, [])
 
     return (
-        <nav className={`${scrolled ? 'shadow-md bg-[#f5ebfe] ' : 'bg-transparent'} ${isDark ? 'bg-accent' : '' } transition-all duration-300 z-100 w-full fixed top-0`}>
+        <nav className={`${pathname === '/' ? 'bg-white' : scrolled ? 'shadow-md bg-[#f5ebfe] ' : 'bg-transparent'} ${isDark ? 'bg-accent' : ''} transition-all duration-300 z-100 w-full fixed top-0`}>
             <div className="relative z-100 w-full  flex">
                 <div className="navbar container px-4 sm:px-6 lg:px-8 mx-auto flex justify-between">
                     <div className="flex items-center gap-20 flex-1">
